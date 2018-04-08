71edcfba3e6607f2a01664e05cb1d3f600b936fc
Tim A. Conner <timaconner1@gmail.com>
2018-04-08 16:19:44 -0500

    Fix accidentally naming the ResetButton the name Tile.

--- Tic Tac Toe/tic-tac-toe/src/ResetButton.jsx
+++ Tic Tac Toe/tic-tac-toe/src/ResetButton.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 
-export default class Tile extends Component {
+export default class ResetButton extends Component {
   renter () {
     return (
       <div className=''>
